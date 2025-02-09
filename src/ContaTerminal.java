80841defda3d754bfc29d9644c9a20f8b01c7a60
Julia Nicola <julia.nicola@sap.com>
2025-02-09 19:21:00 -0300

    feat: adding logic

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
+    public static String name;
+    public static String ag;
+    public static int account;
+    public static float balance;
 
-        System.out.println("g");
+    public static void main(String[] args) {
+        ContaTerminal.print();
+        System.out.println("Olá " + name + ", obrigado por criar uma conta em nosso banco, sua agência é " + ag + ", conta " + account + " e seu saldo " + balance + " já está disponível para saque.");
+
+    }
+    public static void print() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu nome");
+        name = scan.nextLine();
+        System.out.println("Bem-vindo(a), " + name);
+        System.out.println("Digite o número da agência:");
+        ag = scan.nextLine();
+        System.out.println("Digite o número da conta: ");
+        account = scan.nextInt();
+        System.out.println("Digite seu saldo: ");
+        balance = scan.nextFloat();
     }
 
 }
